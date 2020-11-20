3294f69ebca3722b73fcb4b76847998f869ab68e
Uzair <uzair0213@icloud.com>
2020-11-20 13:18:58 -0700

    Update to behave more like a regular bash script
    Still required to be in `.bash_profile` as a function

--- mkvenv.sh
+++ mkvenv.sh
@@ -1,13 +1,40 @@
-GREEN='\033[1;32m'
-NC='\033[0m'
+VERSION="0.1 pre-release"
+
+declare -a ARGS
+args=(
+    "\tenv_name\t\tName of new virtual environment."
+    "\tdir_name\t\tOptional: name for project working directory."
+    "\t-v, --version\t\tPrint the version number of mkvenv."
+    # "\t-l\t\t\tStore output in file"
+)
+
+printUsage() {
+    echo -e "Usage:\n  mkvenv [options] <env_name> [dir_name]"
+    for i in "${args[@]}" ; do
+        echo -e "$i"
+    done
+}
+
 mkvenv() {
-    if [ $# -eq 1 ]
-        then
-            echo "Working..."; virtualenv -q "$1"; cd "$1"; activate; mkdir project; echo -e "${GREEN}Virtualenv $1 created. Use project/ for working.${NC}"
-    elif [ $# -eq 2 ]
-        then
-            virtualenv -q "$1"; cd "$1"; activate; mkdir "$2"; echo -e "${GREEN}Virtualenv $1 created. Use $2/ for working.${NC}"
+    GREEN="\033[1;32m"
+    NC="\033[0m"
+    if [ "$#" -eq 1 ]; then
+        if [ "$1" = "-v" ]  || [ "$1" = "--version" ]; then
+            echo -e "mkvenv ${VERSION}"
+        else
+            virtualenv -q "$1" &&
+            cd "$1" &&
+            source bin/activate &&
+            mkdir "$1Root" &&
+            echo -e "${GREEN}Virtualenv "$1" created. Use "$1Root/" for working.${NC}"
+        fi
+    elif [ "$#" -eq 2 ]; then
+        virtualenv -q "$1" &&
+        cd "$1" &&
+        source bin/activate &&
+        mkdir "$2Root" &&
+        echo -e "${GREEN}Virtualenv "$1" created. Use "$2Root/" for working.${NC}"
     else
-        echo -e "Usage:\n  mkvenv <env_name> [dir_name] \n\tenv_name\tName of new virtual environment. \n\tdir_name\tOptional name for project working directory."
+        printUsage
     fi
 }
